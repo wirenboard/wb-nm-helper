f42a3a1f52633cc3d1489b9c0a557dea89a3107b
lostpoint <lostpoint@icloud.com>
2023-01-25 17:27:40 +0600

    updated tests

--- tests/test_connection_manager_timeouts.py
+++ tests/test_connection_manager_timeouts.py
@@ -1,5 +1,6 @@
 import datetime
 import unittest
+from unittest.mock import patch
 
 from tests.mm_mock import FakeNetworkManager
 from wb.nm_helper.connection_manager import (
@@ -17,78 +18,99 @@ normal_config = {
     }
 }
 
+TEST_NOW = datetime.datetime(year=2000, month=1, day=1)
+
 
 class TimeoutManagerTests(unittest.TestCase):
-    def test_touch_connection_retry_timeout(self):
+
+    def setUp(self) -> None:
 
         config = ConnectionManagerConfigFile(normal_config)
-        tm = TimeoutManager(config)
-        tm.touch_connection_retry_timeout("wb-eth0")
-        timeout = CONNECTION_ACTIVATION_RETRY_TIMEOUT.total_seconds()
-        assert type(tm.connection_retry_timeouts.get("wb-eth0")) is datetime.datetime
-        delta = tm.connection_retry_timeouts.get("wb-eth0") - datetime.datetime.now()
-        assert timeout >= delta.total_seconds() >= (timeout - 5)
+        self.tm = TimeoutManager(config)
+        self.nm = FakeNetworkManager()
 
-    def test_reset_connection_retry_timeout(self):
+    def test_01_touch_connection_retry_timeout(self):
 
-        config = ConnectionManagerConfigFile(normal_config)
-        tm = TimeoutManager(config)
-        tm.touch_connection_retry_timeout("wb-eth0")
-        tm.reset_connection_retry_timeout("wb-eth0")
-        assert type(tm.connection_retry_timeouts.get("wb-eth0")) is datetime.datetime
-        delta = tm.connection_retry_timeouts.get("wb-eth0") - datetime.datetime.now()
-        assert delta.total_seconds() <= 0
+        with patch.object(self.tm, "now") as now_mock:
+            now_mock.return_value = TEST_NOW
+            self.tm.touch_connection_retry_timeout("wb-eth0")
+            timeout = CONNECTION_ACTIVATION_RETRY_TIMEOUT.total_seconds()
+            assert isinstance(self.tm.connection_retry_timeouts.get("wb-eth0"), datetime.datetime)
+            delta = self.tm.connection_retry_timeouts.get("wb-eth0") - TEST_NOW
+            assert delta.total_seconds() == timeout
 
-    def test_touch_gsm_timeout(self):
+    def test_02_reset_connection_retry_timeout(self):
 
-        config = ConnectionManagerConfigFile(normal_config)
-        tm = TimeoutManager(config)
-        nm = FakeNetworkManager()
-        nm.add_ethernet("wb-eth0", device_connected=True, connection_state=NM_ACTIVE_CONNECTION_STATE_ACTIVATED)
-        nm.add_gsm("wb-gsm-sim1", device_connected=True, connection_state=NM_ACTIVE_CONNECTION_STATE_ACTIVATED, sim_slot=1)
+        with patch.object(self.tm, "now") as now_mock:
+            now_mock.return_value = TEST_NOW
+            self.tm.touch_connection_retry_timeout("wb-eth0")
+            self.tm.reset_connection_retry_timeout("wb-eth0")
+            assert isinstance(self.tm.connection_retry_timeouts.get("wb-eth0"), datetime.datetime)
+            delta = self.tm.connection_retry_timeouts.get("wb-eth0") - TEST_NOW
+            assert delta.total_seconds() == 0
 
-        active_cn = nm.get_active_connections().get("wb-eth0")
-        tm.touch_gsm_timeout(active_cn)
-        assert tm.deny_sim_switch_until is None
+    def test_03_touch_gsm_timeout(self):
 
-        active_cn = nm.get_active_connections().get("wb-gsm-sim1")
-        tm.touch_gsm_timeout(active_cn)
-        assert type(tm.deny_sim_switch_until) is datetime.datetime
-        delta = tm.deny_sim_switch_until - datetime.datetime.now()
-        assert 901 > delta.total_seconds() > 890
+        with patch.object(self.tm, "now") as now_mock:
+            now_mock.return_value = TEST_NOW
 
-        active_cn = nm.get_active_connections().get("wb-eth0")
-        tm.touch_gsm_timeout(active_cn)
-        assert tm.deny_sim_switch_until is None
+            self.nm.add_ethernet("wb-eth0", device_connected=True, connection_state=NM_ACTIVE_CONNECTION_STATE_ACTIVATED)
+            self.nm.add_gsm("wb-gsm-sim1", device_connected=True, connection_state=NM_ACTIVE_CONNECTION_STATE_ACTIVATED,
+                            sim_slot=1)
 
-    def test_connection_retry_timeout_is_active(self):
+            active_cn = self.nm.get_active_connections().get("wb-eth0")
+            self.tm.touch_gsm_timeout(active_cn)
+            assert self.tm.deny_sim_switch_until is None
 
-        config = ConnectionManagerConfigFile({})
-        tm = TimeoutManager(config)
+            active_cn = self.nm.get_active_connections().get("wb-gsm-sim1")
+            self.tm.touch_gsm_timeout(active_cn)
+            assert isinstance(self.tm.deny_sim_switch_until, datetime.datetime)
+            delta = self.tm.deny_sim_switch_until - TEST_NOW
+            assert delta.total_seconds() == self.tm.config.sticky_sim_period.total_seconds()
 
-        tm.connection_retry_timeouts = {}
-        assert tm.connection_retry_timeout_is_active("wb-eth0") is False
-        assert tm.connection_retry_timeout_is_active("wb-gsm-sim1") is False
+            active_cn = self.nm.get_active_connections().get("wb-eth0")
+            self.tm.touch_gsm_timeout(active_cn)
+            assert self.tm.deny_sim_switch_until is None
 
-        tm.connection_retry_timeouts = {
-            "wb-eth0": datetime.datetime.now() - CONNECTION_ACTIVATION_RETRY_TIMEOUT
-        }
-        assert tm.connection_retry_timeout_is_active("wb-eth0") is False
-        assert tm.connection_retry_timeout_is_active("wb-gsm-sim1") is False
+    def test_04_connection_retry_timeout_is_active__empty(self):
+        with patch.object(self.tm, "now") as now_mock:
+            now_mock.return_value = TEST_NOW
+            self.tm.connection_retry_timeouts = {}
+            assert self.tm.connection_retry_timeout_is_active("wb-eth0") is False
+            assert self.tm.connection_retry_timeout_is_active("wb-gsm-sim1") is False
 
-        tm.connection_retry_timeouts = {
-            "wb-eth0": datetime.datetime.now() + CONNECTION_ACTIVATION_RETRY_TIMEOUT
-        }
-        assert tm.connection_retry_timeout_is_active("wb-eth0") is True
-        assert tm.connection_retry_timeout_is_active("wb-gsm-sim1") is False
+    def test_05_connection_retry_timeout_is_active__expired(self):
+        with patch.object(self.tm, "now") as now_mock:
+            now_mock.return_value = TEST_NOW
+            self.tm.connection_retry_timeouts = {
+                "wb-eth0": TEST_NOW - CONNECTION_ACTIVATION_RETRY_TIMEOUT
+            }
+            assert self.tm.connection_retry_timeout_is_active("wb-eth0") is False
+            assert self.tm.connection_retry_timeout_is_active("wb-gsm-sim1") is False
 
-    def test_gsm_sticky_timeout_is_active(self):
+    def test_06_connection_retry_timeout_is_active__armed(self):
+        with patch.object(self.tm, "now") as now_mock:
+            now_mock.return_value = TEST_NOW
+            self.tm.connection_retry_timeouts = {
+                "wb-eth0": TEST_NOW + CONNECTION_ACTIVATION_RETRY_TIMEOUT
+            }
+            assert self.tm.connection_retry_timeout_is_active("wb-eth0") is True
+            assert self.tm.connection_retry_timeout_is_active("wb-gsm-sim1") is False
 
-        config = ConnectionManagerConfigFile(normal_config)
-        tm = TimeoutManager(config)
+    def test_07_gsm_sticky_timeout_is_active__empty(self):
+        with patch.object(self.tm, "now") as now_mock:
+            now_mock.return_value = TEST_NOW
+            self.tm.deny_sim_switch_until = None
+            assert self.tm.gsm_sticky_timeout_is_active() is False
 
-        tm.deny_sim_switch_until = None
-        assert tm.gsm_sticky_timeout_is_active() is False
+    def test_08_gsm_sticky_timeout_is_active__expired(self):
+        with patch.object(self.tm, "now") as now_mock:
+            now_mock.return_value = TEST_NOW
+            self.tm.deny_sim_switch_until = TEST_NOW - self.tm.config.sticky_sim_period
+            assert self.tm.gsm_sticky_timeout_is_active() is False
 
-        tm.deny_sim_switch_until = datetime.datetime.now() + tm.config.sticky_sim_period
-        assert tm.gsm_sticky_timeout_is_active() is True
+    def test_09_gsm_sticky_timeout_is_active__active(self):
+        with patch.object(self.tm, "now") as now_mock:
+            now_mock.return_value = TEST_NOW
+            self.tm.deny_sim_switch_until = TEST_NOW + self.tm.config.sticky_sim_period
+            assert self.tm.gsm_sticky_timeout_is_active() is True
