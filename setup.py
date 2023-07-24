c9458d99ae656fa15f60504f2bbf1c58b538b0f2
Nikolay Korotkiy <sikmir@disroot.org>
2023-07-24 14:47:41 +0400

    Fix pkg-info

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@
 
 from setuptools import setup
 
+
+def get_version():
+    with open("debian/changelog", "r", encoding="utf-8") as f:
+        return f.readline().split()[1][1:-1]
+
+
 setup(
     name="wb-nm-helper",
-    version="1.0",
+    version=get_version(),
     description="wb-mqtt-confed backend for network configuration",
+    license="MIT",
     author="Petr Krasnoshchekov",
     author_email="petr.krasnoshchekov@wirenboard.ru",
+    maintainer="Wiren Board Team",
+    maintainer_email="info@wirenboard.com",
+    url="https://github.com/wirenboard/wb-nm-helper",
     packages=["wb.nm_helper"],
 )
